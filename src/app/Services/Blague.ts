4092fbacae6b28ec10d92da49b0bd6a374791453
Lioxyze <160881557+Lioxyze@users.noreply.github.com>
2024-12-12 21:08:37 +0100

    Update Blague.ts

--- src/app/Services/Blague.ts
+++ src/app/Services/Blague.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const fetchAllBlagues = async () => {
   try {
-    const response = await axios.get("http://localhost:3000/blague");
+    const response = await axios.get(
+      "https://malabar-a2097f13b22e.herokuapp.com/blague"
+    );
     return response.data.blagues;
   } catch (error) {
     console.error("Error fetching blagues:", error);
